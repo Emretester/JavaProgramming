aa07ddef32751f346c333cb7cf1629bd93a4fb62
Emretester <emreyeni2019@gmail.com>
2023-01-18 16:00:20 +0300

    day44_Abstraction animalTask Animal.java

--- src/day44_Abstraction/animalTask/Animal.java
+++ src/day44_Abstraction/animalTask/Animal.java
@@ -84,8 +84,6 @@ public abstract class Animal {
 
     public abstract void eat();
 
-
-    @Override
     public String toString() {
         return getClass().getSimpleName() + "{" +
                 "name='" + name + '\'' +
